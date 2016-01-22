1b69cd69af608dc24998191078ad340c2a82dfc9
scottorama1 <scottorama1@users.noreply.github.com>
2016-01-22 12:54:16 +1100

    Update f5.ps1
    updated to include multiple load balancers from a flat text file - and determine if they're active or not (i.e. failover pairs)

--- f5.ps1
+++ f5.ps1
@@ -1,10 +1,38 @@
 Add-PSSnapin iControlSnapIn
 
 $script = $MyInvocation.MyCommand.Name
-$output_path = "c:\Users\Desktop\"
-$output_file="$script-out.txt"
-$last_run_file = "$script-out-last-run.txt"
-$device = "xxxx"
+$output_path = "c:\Users\xxx\Desktop\"
+$config_file = "$output_path\f5.config"
+$username = "xxx"
+$password = "xxx"
+
+# define this as an arraylist so that items can be removed from it (i.e. it's not fixed length)
+[System.Collections.ArrayList]$device_list = Get-Content $config_file
+
+#work through the list assessing the failover status of each device
+# This creates an array of only the active devices.
+# While I could create a $devicelogin for each device, it's easier just to use the current credentials and re-authenticate
+
+$active_device = @()
+foreach ($device in $device_list)
+{
+    # log in to the device
+    $login = Initialize-F5.iControl -Hostname $device -Username $username -Password $password
+
+    # Check to make sure we are on the active device.
+    $failover_state = Get-F5.DBVariable | Where {$_.Name -eq "Failover.State"} | Select Value
+
+    if ($failover_state.Value -eq "active")
+    {
+        $active_device+=$device
+        write-host -ForegroundColor DarkGreen $device + "is currently active."
+    }
+    else
+    {
+        write-host -ForegroundColor Red $device + "is not currently active."
+    }
+} #end of foreach loop
+
 
 $style = "<style>BODY{font-family: Arial; font-size: 10pt;}"
 $style = $style + "TABLE{border: 1px solid black; border-collapse: collapse;}"
@@ -13,100 +41,108 @@ $style = $style + "TD{border: 1px solid black; padding: 5px; }"
 $style = $style + "</style>"
 
 
-if ( Test-Path -Path $output_path\$last_run_file )
+
+foreach ($device in $active_device)
 {
-    write-host "Removing Previous Last run."
-    Remove-Item $output_path\$last_run_file
-}
 
-if ( Test-Path $output_path\$output_file )
-{
-    write-host "Renaming current run to previous run"
-    Rename-Item $output_path\$output_file $output_path\$last_run_file
-}
+    Write-host -ForegroundColor Yellow Processing $device
+    $output_file="$script-$device-out.txt"
+    $last_run_file = "$script-$device-out-last-run.txt"
 
-$ErrorActionPreference= 'silentlycontinue'
-Try
-{
-    #$login = Initialize-F5.iControl -Hostname $device -Username "asdasdas" -Password "asdsadas"
-    $login = Initialize-F5.iControl -Hostname $device -Credentials (Get-Credential -Message "Enter your F5 username and password")
-}
-Catch [Exception]
-{
-    Write-host "An Error occurred"
-    exit 1;
-}
-
-if ($login -ne "True") 
-{ 
-    Write-host -foregroundcolor Red "[ERROR: 2] Login not correct."
-    exit 2;
-}
-
-$ErrorActionPreference= 'continue'
-
-# Check to make sure we are on the active device.
-$failover_state = Get-F5.DBVariable | Where {$_.Name -eq "Failover.State"} | Select Value
-
-if ($failover_state.Value -ne "active")
-{
-    Write-Host -ForegroundColor  Red "[ERROR: 3] The device " $device " is currently in state" $failover_state.Value " - please change the device to be the active node and try again."
-    exit 3;
-}
-
-
-#Get-F5.iControlCommands
-
-$virtuals = Get-F5.LTMVirtualServer
-
-write-host $virtuals.count " virtual servers need to be processed."
-
-$ic = Get-F5.iControl
-#Set script to top level folder so that enumerated folders are from the root.
-$ic.SystemSession.set_active_folder("/")
-
-# Get the active number of partitions on the device.
-#Split these in to an array so that each is individually addressable
-#$partitions_array[1] is the second element
-
-$partitions = $ic.ManagementFolder.get_list()
-
-# Open a file and write a header in to the file.
-# header is important when we later use import-csv to import directly to an array.
-$file = New-Object System.IO.StreamWriter $output_path\$output_file
-$file.WriteLine("VIP,IP,PORT,STATISTIC,VALUE")
-
-foreach ($element in $partitions)
-{
-    write-host "Processing Partition: " $element 
-    $ic.SystemSession.set_active_folder($element)
-    $virtual_servers = Get-F5.LTMVirtualServer
-    foreach ($item in $virtual_servers)
+    if ( Test-Path -Path $output_path\$last_run_file )
     {
-        if ($virtual_servers.count -ne 0) 
+        write-host "Removing Previous Last run."
+        Remove-Item $output_path\$last_run_file
+    }
+
+    if ( Test-Path $output_path\$output_file )
+    {
+        write-host "Renaming current run to previous run"
+        Rename-Item $output_path\$output_file $output_path\$last_run_file
+    }
+
+    $ErrorActionPreference= 'silentlycontinue'
+    Try
+    {
+        $login = Initialize-F5.iControl -Hostname $device -Username $username -Password $password
+        #$login = Initialize-F5.iControl -Hostname $device -Credentials (Get-Credential -Message "Enter your F5 username and password")
+    }
+    Catch [Exception]
+    {
+        Write-host "An Error occurred"
+        exit 1;
+    }
+
+    if ($login -ne "True") 
+    { 
+        Write-host -foregroundcolor Red "[ERROR: 2] $device Login not correct."
+        exit 2;
+    }
+
+    $ErrorActionPreference= 'continue'
+
+
+    # Check to make sure we are on the active device.
+    # this should not be necessary as we have an array of only active devices
+    $failover_state = Get-F5.DBVariable | Where {$_.Name -eq "Failover.State"} | Select Value
+
+    if ($failover_state.Value -ne "active")
+    {
+        Write-Host -ForegroundColor  Red "[ERROR: 3] The device " $device " is currently in state" $failover_state.Value " - please change the device to be the active node and try again."
+        exit 3;
+    }
+
+
+    $virtuals = Get-F5.LTMVirtualServer
+
+    write-host $virtuals.count " virtual servers need to be processed."
+
+    $ic = Get-F5.iControl
+    #Set script to top level folder so that enumerated folders are from the root.
+    $ic.SystemSession.set_active_folder("/")
+
+    # Get the active number of partitions on the device.
+    #Split these in to an array so that each is individually addressable
+    #$partitions_array[1] is the second element
+
+    $partitions = $ic.ManagementFolder.get_list()
+
+    # Open a file and write a header in to the file.
+    # header is important when we later use import-csv to import directly to an array.
+    $file = New-Object System.IO.StreamWriter $output_path\$output_file
+    $file.WriteLine("VIP,IP,PORT,STATISTIC,VALUE")
+
+    foreach ($element in $partitions)
+    {
+        write-host "Processing Partition: " $element 
+        $ic.SystemSession.set_active_folder($element)
+        $virtual_servers = Get-F5.LTMVirtualServer
+        foreach ($item in $virtual_servers)
         {
-            $virtual_servers_statistics = Get-F5.LTMVirtualServerStatistics $item.Name
-            if ( $output )
+            if ($virtual_servers.count -ne 0) 
             {
-                write-host $item.Name `n`t $virtual_servers_statistics.Statistics[10].Type $virtual_servers_statistics.Statistics[10].Value
-            }
-            $virt_address = $ic.LocalLBVirtualServer.get_destination($item.name)
-            $file.WriteLine($item.Name + "," + $virt_address[0].address + "," + $virt_address.port + "," + $virtual_servers_statistics.Statistics[10].Type + "," + $virtual_servers_statistics.Statistics[10].Value)
-            write-host -nonewline "."
-        } #end of if statement
-    } #end of foreach loop virtual_servers
+                $virtual_servers_statistics = Get-F5.LTMVirtualServerStatistics $item.Name
+                if ( $output )
+                {
+                    write-host $item.Name `n`t $virtual_servers_statistics.Statistics[10].Type $virtual_servers_statistics.Statistics[10].Value
+                }
+                $virt_address = $ic.LocalLBVirtualServer.get_destination($item.name)
+                $file.WriteLine($item.Name + "," + $virt_address[0].address + "," + $virt_address.port + "," + $virtual_servers_statistics.Statistics[10].Type + "," + $virtual_servers_statistics.Statistics[10].Value)
+                write-host -nonewline "."                
+            } #end of if statement           
+        } #end of foreach loop virtual_servers
+        write-host
+    }
 
-}
+    $file.Close();
 
-$file.Close();
+    #import the last run date and the current run then produce an array of the differences.
 
-#import the last run date and the current run then produce an array of the differences.
-
-if ( Test-Path -Path $output_path\$last_run_file )
-{
-    $last_run = import-csv $output_path\$last_run_file
-}
-$current_run = import-csv $output_path\$output_file
+    if ( Test-Path -Path $output_path\$last_run_file )
+    {
+        $last_run = import-csv $output_path\$last_run_file
+    }
+    $current_run = import-csv $output_path\$output_file
 
 #if ( Test-Path $output_path\$last_run_file )
 #{
@@ -117,26 +153,27 @@ $current_run = import-csv $output_path\$output_file
 #}
 
 
-# create a custom object that can be added to an array with current and previous values.
-# much neater way of doing this because it means that I can use a custom object to contain only the values I need.
-$array = @()
-foreach ($item in $last_run)
-{
-    $object = New-Object -TypeName PSObject
-    $object | Add-Member -Name 'VIP' -MemberType NoteProperty -Value $item.VIP
-    $object | Add-Member -Name 'IP' -MemberType NoteProperty -Value $item.IP
-    $object | Add-Member -Name 'PORT' -MemberType NoteProperty -Value ([int64]$item.PORT)
-    $object | Add-Member -Name 'PreviousTotalConnections' -MemberType NoteProperty -Value ([int64]$item.VALUE)
-    $current_value = $current_run | where {$_.VIP -eq $item.VIP} | select VALUE
-    $object | Add-Member -Name 'CurrentTotalConnections' -MemberType NoteProperty -Value ([int64]$current_value.VALUE)
-    $difference = $object.CurrentTotalConnections - $object.PreviousTotalConnections
-    $object | Add-Member -Name 'Difference' -MemberType NoteProperty -Value ([int64]$difference)
-    $array += $object   
-}
+    # create a custom object that can be added to an array with current and previous values.
+    # much neater way of doing this because it means that I can use a custom object to contain only the values I need.
+    $array = @()
+    foreach ($item in $last_run)
+    {
+        $object = New-Object -TypeName PSObject
+        $object | Add-Member -Name 'VIP' -MemberType NoteProperty -Value $item.VIP
+        $object | Add-Member -Name 'IP' -MemberType NoteProperty -Value $item.IP
+        $object | Add-Member -Name 'PORT' -MemberType NoteProperty -Value ([int64]$item.PORT)
+        $object | Add-Member -Name 'PreviousTotalConnections' -MemberType NoteProperty -Value ([int64]$item.VALUE)
+        $current_value = $current_run | where {$_.VIP -eq $item.VIP} | select VALUE
+        $object | Add-Member -Name 'CurrentTotalConnections' -MemberType NoteProperty -Value ([int64]$current_value.VALUE)
+        $difference = $object.CurrentTotalConnections - $object.PreviousTotalConnections
+        $object | Add-Member -Name 'Difference' -MemberType NoteProperty -Value ([int64]$difference)
+        $array += $object   
+    }
 
-# loop through my new custom array and only get the ones that have increased.
+    # loop through my new custom array and only get the ones that have increased.
+    $array | where {$_.PreviousTotalConnections -ne $_.CurrentTotalConnections} | ConvertTo-Html -Head $style > $output_path\$script-$device-differences.html
+    $array | ConvertTo-Html -Head $style > $output_path\$script-$device-totals.html
 
-
-$array | where {$_.PreviousTotalConnections -ne $_.CurrentTotalConnections}
-$array | where {$_.PreviousTotalConnections -ne $_.CurrentTotalConnections} | ConvertTo-Html -Head $style > $output_path\$script-differences.html
-$array | ConvertTo-Html -Head $style > $output_path\$script-totals.html
+    Write-host -ForegroundColor Yellow Completed Processing $device
+    write-host "--------------------------------------------------------"
+} #end device foreach
